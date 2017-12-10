d9b3aa7e6f48be738b36476b87dc21509dff11c7
FNS <32462999+fnsneuralsimulator@users.noreply.github.com>
2017-12-10 01:01:49 +0100

    Update Node.java

--- src/connectivity/nodes/Node.java
+++ src/connectivity/nodes/Node.java
@@ -1,26 +1,33 @@
 /**
- *  Copyright 2015-2016 Emanuele Paracone - knizontes
- *  knizontes_æŧ_gmail.com
- *  emanuele.paracone_æŧ_gmail.com
- *  emanuele.paracone_æŧ_hpe.com
- *  https://github.com/knizontes
- *  http://knizontes.ns0.it
- *  
- *  This file is part of k-fren.
- *
- *  k-fren is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  k-fren is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with k-fren.  If not, see <http://www.gnu.org/licenses/>.
- */
+* This file is part of FNS (Firnet NeuroScience), ver.1.0.1
+*
+* (c) 2017, Mario Salerno, Gianluca Susi, Alessandro Cristini, Emanuele Paracone.
+*
+* CITATION:
+* When using FNS for scientific publications, cite us as follows:
+*
+* Gianluca Susi, Pilar Garcés, Alessandro Cristini, Emanuele Paracone, Mario 
+* Salerno, Fernando Maestú, Ernesto Pereda (2018). "FNS: An event-driven spiking 
+* neural network framework for efficient simulations of large-scale brain 
+* models". 
+* Laboratory of Cognitive and Computational Neuroscience, UPM-UCM Centre for 
+* Biomedical Technology, Technical University of Madrid; University of Rome "Tor 
+* Vergata".   
+* Paper under review.
+*
+* FNS is free software: you can redistribute it and/or modify it under the terms 
+* of the GNU General Public License version 3 as published by  the Free Software 
+* Foundation.
+*
+* FNS is distributed in the hope that it will be useful, but WITHOUT ANY 
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR 
+* A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License along with 
+* FNS. If not, see <http://www.gnu.org/licenses/>.
+* -----------------------------------------------------------
+* Website:   http://www.fnsneuralsimulator.org
+*/
 
 
 package connectivity.nodes;
