fed59c64cfdb3d9385087853edecc8aba6200dff
knizontes@gmail.com <knizontes@gmail.com>
2019-10-22 03:10:14 +0200

    d_exc-d_inh

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-java -Xmx1024m -Dfile.encoding=Cp1252 -jar target\fns-V2.14.6-jar-with-dependencies.jar %*
+java -Xmx1024m -Dfile.encoding=Cp1252 -jar target\fns-V2.15.0-jar-with-dependencies.jar %*
